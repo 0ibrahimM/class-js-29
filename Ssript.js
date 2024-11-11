3081a3c784f4be86a454c0880ce46788377995d0
0ibrahimM <meshkat271@gmail.com>
2024-11-11 01:33:42 -0800

    array

---
+++ Ssript.js
@@ -0,0 +1,70 @@
+
+
+let Array =[98,63,69,39,69,85,58,74,95,48,];
+
+
+console.log(Array [0]);
+console.log(Array [9]);
+console.log(Array [2]);
+console.log(Array [5]);
+console.log(Array [3]);
+
+
+let student={name:"ibrahim", age:25,phone:1787937954,blood:"b+",email:"meshkat271@gmail.com",};
+console.log(student.email);
+console.log(student.name);
+console.log(student.age);
+
+console.log(student.phone);
+
+let twodimensionalarray=[
+[58,85,65,56,52],
+[12,21,25,54,45],
+[80,7,90,22,38],
+[51,14,67,74,42],
+[120,150,220,300,450],
+];
+console.log(twodimensionalarray[0][0]);
+console.log(twodimensionalarray[0][1]);
+console.log(twodimensionalarray[0][2]);
+console.log(twodimensionalarray[0][3]);
+console.log(twodimensionalarray[0][4]);
+
+
+console.log(twodimensionalarray[1][0]);
+console.log(twodimensionalarray[1][1]);
+console.log(twodimensionalarray[1][2]);
+console.log(twodimensionalarray[1][3]);
+console.log(twodimensionalarray[1][4]);
+
+
+console.log(twodimensionalarray[2][0]);
+console.log(twodimensionalarray[2][1]);
+console.log(twodimensionalarray[2][2]);
+console.log(twodimensionalarray[2][3]);
+console.log(twodimensionalarray[2][4]);
+
+console.log(twodimensionalarray[3][0]);
+console.log(twodimensionalarray[3][1]);
+console.log(twodimensionalarray[3][2]);
+console.log(twodimensionalarray[3][3]);
+console.log(twodimensionalarray[3][4]);
+
+console.log(twodimensionalarray[4][0]);
+console.log(twodimensionalarray[4][1]);
+console.log(twodimensionalarray[4][2]);
+console.log(twodimensionalarray[4][3]);
+console.log(twodimensionalarray[4][4]);
+
+
+
+
+
+
+
+
+
+
+
+
+
